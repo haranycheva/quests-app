25c08d75d3886e7732d034283b6f916f49fc1a0f
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-02-12 22:59:16 +0200

    edit quest card styles

--- src/components/repeated/QuestInfoCard/QuestInfoCard.jsx
+++ src/components/repeated/QuestInfoCard/QuestInfoCard.jsx
@@ -6,33 +6,33 @@ import Link from "next/link";
 
 export const QuestInfoCard = ({ quest }) => {
   return (
-<Link className="w-11/12 max-w-screen-md" href={`/quests/${quest.id}`}>
-        <div className="flex  p-3 bg-basic-color rounded-lg items-center">
-          <div className="relative w-24 h-24 rounded-lg overflow-hidden">
-            <Image
-              width="96"
-              height="96"
-              src={quest.posterImage}
-              alt={quest.name}
-              priority={false}
-              placeholder="blur"
-              blurDataURL={rgbDataURL(230, 230, 230)}
-            />
-          </div>
-          <div className="ml-3 flex justify-between flex-1">
-            <div>
-              <h4
-                className={`${barrio.className} text-2xl font-semibold text-accent-color`}
-              >
-                {quest.name}
-              </h4>
-              <p className="text-lg font-semibold text-accent-color">
-                {quest.category.toLowerCase()}
-              </p>
-            </div>
-            {quest.rating ? <Ratings rating={quest.rating} /> : <></>}
-          </div>
+    <Link className="w-11/12 max-w-screen-md" href={`/quests/${quest.id}`}>
+      <div className="flex  p-3 bg-basic-color rounded-lg items-center">
+        <div className="relative w-24 h-24 rounded-lg overflow-hidden">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={quest.posterImage}
+            alt={quest.name}
+            priority={false}
+            placeholder="blur"
+            blurDataURL={rgbDataURL(230, 230, 230)}
+          />
         </div>
-</Link>
+        <div className="ml-3 flex justify-between flex-1">
+          <div>
+            <h4
+              className={`${barrio.className} text-2xl font-semibold text-accent-color`}
+            >
+              {quest.name}
+            </h4>
+            <p className="text-lg font-semibold text-accent-color">
+              {quest.category.toLowerCase()}
+            </p>
+          </div>
+          {quest.rating ? <Ratings rating={quest.rating} /> : <></>}
+        </div>
+      </div>
+    </Link>
   );
 };

5262afde1f82b8c8dd97cbaa74ac9eeb21448c7a
iveke <iven.tkachenkoi@gmail.com>
2025-02-12 22:11:26 +0200

    feat: questList, functional

--- src/components/repeated/QuestCard/QuestCard.jsx
+++ src/components/repeated/QuestCard/QuestCard.jsx
@@ -2,22 +2,20 @@ import Link from "next/link";
 import { Ratings } from "./Ratings";
 import Image from "next/image";
 
-export function QuestCard({ quest}) {
+export function QuestCard({ quest }) {
   return (
     <Link className="block" href="#">
-        <div className="flex items-center flex-col">
-            <Image
-              width="208"
-              height="278"
-              src={quest.photo}
-              alt="poster"
-              className="w-52 h-62"
-            />
-            <h4 className="text-[#011627] font-inter text-xl">
-              {quest.name}
-            </h4>
-            <Ratings rating={quest.rating}/>
-        </div>
+      <div className="flex items-center flex-col">
+        <Image
+          width="208"
+          height="278"
+          src={quest.posterImage}
+          alt="poster"
+          className="w-52 h-62"
+        />
+        <h4 className="text-[#011627] font-inter text-xl">{quest.name}</h4>
+        {quest.rating && <Ratings rating={quest.rating} />}
+      </div>
     </Link>
   );
 }
